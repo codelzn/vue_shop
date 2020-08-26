10b0c33cf360dc3be324520716f4d723332d9b22
codelzn <724958732@qq.com>
2020-08-26 14:16:12 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -4,6 +4,18 @@ import router from './router'
 import store from './store'
 import './plugins/element.js'
 
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+
+// 导入全局样式表
+import './assets/css/global.css'
+
+// 导入axios
+import axios from 'axios'
+// 配置请求根路径
+axios.defaults.baseURL = 'http://112.124.12.46/api/private/v1/'
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
 
 new Vue({
