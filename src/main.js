21a5cf298e88039754d60144cbf8b8406713ce02
codelzn <724958732@qq.com>
2020-08-28 11:41:57 +0800

    商品列表完成

--- src/main.js
+++ src/main.js
@@ -13,6 +13,13 @@ import './assets/css/global.css'
 
 // 导入axios
 import axios from 'axios'
+
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css'
 Vue.component('tree-table', TreeTable)
 // 配置请求根路径
 axios.defaults.baseURL = 'http://112.124.12.46/api/private/v1/'
@@ -24,6 +31,20 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+// 全局时间过滤器
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, 0)
+  const hh = (dt.getHours() + '').padStart(2, 0)
+  const mm = (dt.getMinutes() + '').padStart(2, 0)
+  const ss = (dt.getSeconds() + '').padStart(2, 0)
+  return `${y}-${m}-${d}-${hh}-${mm}-${ss}`
+}) // for bubble theme
+
+Vue.use(VueQuillEditor /* { default global options } */)
 
 new Vue({
   router,
